83ea3b477c10c757e760f565183557bea2330fd3
BrendonHawkins <artbitcodedata@gmail.com>
2021-10-01 12:15:28 -0400

    Adding the projects endpoints layout, not fixinf middleware

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,51 @@
-// add middlewares here related to projects
+const User = require('./projects-model')
+
+
+async function validateUserId(req, res, next) {
+try {
+    const user = await User.getById(req.params.id)
+     if(!user){
+        res.status(404).json({
+          message: "user not found"
+        })
+     } else {
+        req.user = user
+        next()
+     }
+} catch (err){
+  res.status(500).json({
+    message: "problem finding user"
+  })
+}
+}
+
+function validateUser(req, res, next) {
+const {name} = req.body
+if(!name|| !name.trim()){
+  res.status(400).json({
+    message: 'missing required name field'
+  })
+}else{
+  req.name = name.trim()
+  next()
+}
+}
+
+function validatePost(req, res, next) {
+  const {text} = req.body
+  if(!text|| !text.trim()){
+    res.status(400).json({
+      message: 'missing required text field'
+    })
+  }else{
+    req.text = text.trim()
+    next()
+  }
+}
+
+ 
+module.exports = {
+validateUserId,
+validateUser,
+validatePost
+}
--- api/server.js
+++ api/server.js
@@ -1,9 +1,42 @@
-const express = require('express');
+const express = require('express'); 
+const helmet = require('helmet'); 
+const projectsRouter = require('./projects/projects-router');
+// const actionsRouter = require('./actions/actions-router');
+
 const server = express();
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+server.use(express.json());
+server.use(helmet()); 
+server.use('/api/projects', logger, projectsRouter);
+// server.use('/api/actions', logger, actionsRouter);
+
+// server.get('/', logger, (req, res) => {
+//   res.send(`
+//     <h2>Lambda Hubs API</h2>
+//     <p>Welcome to the Lambda Hubs API</p>
+//   `);
+// });
+
+server.use('*', logger, (req, res, next) => {
+  next({ status: 404, message: `${req.method} ${req.originalUrl} not found!` })
+});
+
+server.use(errorHandling)
 
 module.exports = server;
+
+
+function logger(req, res, next) {
+    const timeStamp = new Date().toLocaleString()
+    const method = req.method
+    const url = req.originalUrl
+    console.log(` [${timeStamp}] ${method} to ${url}`)
+    next()
+  }
+
+// eslint-disable-next-line
+function errorHandling(err, req, res, next) { 
+  res.status(err.status || 500).json({
+    message: err.message,
+  })
+}
